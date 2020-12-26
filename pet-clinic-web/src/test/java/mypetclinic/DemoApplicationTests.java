ca3d030f0dc0695d1a9ef0cb7093fc0b4d2b8b1b
Arash Goodarzi <aras.goodarzi@gmail.com>
2020-12-25 23:24:32 -0500

    refactoring to multimodule maven project. close #1

---
+++ pet-clinic-web/src/test/java/mypetclinic/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package mypetclinic;
+
+import adak.springframework.mypetclinic.DemoApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
